081857bd18c1c31f3c8fa167cf147b515c4ae625
sachan12 <sachinnanda427@gmail.com>
2024-03-11 12:18:45 -0700

    adds  files

---
+++ main.ts
@@ -0,0 +1,58 @@
+// Practice Session -8 | Increment | Decrement ;
+// 1 - Increment
+// let x = 10;
+// // x= x + 1;
+// ++x;           // (++ means x kii value mein 1 add hoga;)
+// console.log(x);
+
+//  let y = 200;
+// // Pre - increment
+//   console.log(++y);  // 201
+//   console.log(++y);  // 202
+//   console.log(y);   // 202
+
+// let num1 = ++y ;
+// console.log(num1);  // 201
+
+// Post - increment (Post means baad mai increment hoga;)
+// y++;
+// console.log(y++); //200
+// console.log(y++);  // 201
+// console.log(y); // 202
+// let num2 = y++;
+// console.log(num2); // 200
+// num2 = y;
+// console.log (num2);  // 201
+
+// 2 - Decrement 
+// let x = 10;
+// x = x-1
+// console.log(x);    //9
+
+
+// Pre - Decrement 
+// --x
+// console.log(--x);   //9
+
+
+// Post-Decrement 
+// x--;
+// console.log(x);
+// console.log(x--);   // 10
+// console.log(x);     // 9
+
+
+// Generally Practice;
+// let num1 = 20;
+// console.log((++num1)+ (++num1) );   // 43 (Pre-Increment);
+//            21    +    22      =     43
+//console.log((num1++)+ (num1++) + num1 );      //  63 (Post-Increment); 
+//            20     + 21      + 22     =       63
+
+// console.log((++num1)+ (num1++)+(num1) );  // 64
+//            21     +  21     + 22    ; // 64
+
+// console.log((num1++)+(--num1) - (++num1) );   // 19
+//            20    +  20     -   21      ;  // 19 
+
+
